e03c447ae7114f20427dd072299bc07deb195cb4
nffgnfg <djfduvbxs@gmail.com>
2022-07-17 22:31:33 +0300

    u

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,3 @@
 module.exports = ({ env }) => ({
-  url: env("MY_HEROKU_URL"),
-  proxy: true,
-  app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
-  },
+    url: env('MY_HEROKU_URL'),
 });
